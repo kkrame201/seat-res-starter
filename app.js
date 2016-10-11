08093f729134895044a89f372fb36726ceb4f8cf
Kurt <Kurt>
2016-10-10 22:22:25 -0400

    Added JS functionality for reserving seats

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 console.log("Hello, World!");
+
+
+function reserveSeat(reserved) {
+	if (reserved) {
+		alert('Sorry, this seat is already reserved.');
+		document.getElementById("contact-form").innerHTML = " ";
+}
+
+	else {
+		location.href = "#contact-form";
+		document.getElementById("contact-form").innerHTML = '<form id="reserveSeatForm"><label>Name:  <input type="name" name="name" placeholder="Your Name"></input></label></ br><label>Email:  <input type="email" name="email-address" placeholder="johnsmith@abc.com"></input></label><button type="sumbit" onclick="submitForm()">Submit</button></form>';
+	}
+}
+
+
+function submitForm() {
+	alert('Your seat has been reserved!');
+	document.getElementById("contact-form").innerHTML = " ";
+}
